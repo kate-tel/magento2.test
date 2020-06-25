9778953f78b3d380e88ae025b3829fed69820083
ktelesh@gmail.com <ktelesh@gmail.com>
2020-06-25 16:10:52 +0200

    Initial commit

---
+++ magento/mag_object_cart.py
@@ -0,0 +1,13 @@
+class Cart(object):
+    edit = 'a[class="action action-edit"]'
+    quantity_field = 'input.input-text.qty'
+    delete_item = 'a[class="action action-delete"]'
+    is_upgraded = 'span[class="counter qty"]'
+    subtotal = 'span[class="price-wrapper"]>span'
+    quantity = 'input[class="item-qty cart-item-qty"]'
+    update = 'button[class="update-cart-item"]'
+    number_of_items = 'div.items-total>span.count'
+    delete = 'a[class="action delete"]'
+    confirm = 'button[class="action-primary action-accept"]'
+    luma_watch_photo = 'img[src="https://magento2.test/media/catalog/product/cache/ff4352095e8e8adacbd70c6685547492/w/g/wg09-gr-0.jpg"]'
+    proceed_to_checkout_button = 'ul[class="checkout methods items checkout-methods-items"]>li>button'
